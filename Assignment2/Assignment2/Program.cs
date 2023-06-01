c7d140f1717b6688db6e4da352d8fef7478b23d8
Rakhi Mestri <rakhi.mestri@neosoftmail.com>
2023-06-01 10:30:32 +0530

    commit

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+ static void Main(string[] args)
+{
+
+    Medicines objmed = new Medicines();
+    objmed.Accept();
+    objmed.Display();
+
+    Polygone objpoly = new Polygone("");
+    objpoly.draw();
+
+    Rectangle objrect = new Rectangle("");
+    objrect.draw();
+
+    Circle objcircle = new Circle();
+    objcircle.getArea(4);
+
+    Rectangle objrect = new Rectangle("");
+    objrect.getArea();
+
+    //e.g. of inedex out of range
+    int[] ar = { 1, 2, 3, 4, 5 };
+
+    // causing exception
+    for (int i = 0; i <= ar.Length; i++)
+        Console.WriteLine(ar[i]);
+
+    Console.WriteLine("Throw Example");
+    try
+    {
+        string empName = null;
+        ThrowException(empName);
+    }
+    catch (ArgumentNullException objnull)
+    {
+        Console.WriteLine(objnull);
+    }
+    lambdaprog t = new lambdaprog(delegate (int i) { return i * i; });
+    Console.WriteLine(t(2));
+
+
+
+
+
+
+
+
+
+
+}
