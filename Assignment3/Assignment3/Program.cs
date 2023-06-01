61c259f4ae388b45d39f07ca37ef4b8e07517061
Rakhi Mestri <rakhi.mestri@neosoftmail.com>
2023-06-01 10:35:58 +0530

    commit

---
+++ Assignment3/Assignment3/Program.cs
@@ -0,0 +1,92 @@
+﻿internal class Assignment3
+{
+    public abstract class Clothing
+    {
+        public abstract void Sales();
+
+        public abstract void Materials();
+
+        public abstract void CommonWear();
+    }
+
+    public class TrendSetting : Clothing
+    {
+        public override void Sales()
+        {
+            Console.WriteLine("Sales ");
+
+        }
+
+        public override void Materials()
+        {
+            Console.WriteLine("Materials");
+
+        }
+
+        public override void CommonWear()
+        {
+            Console.WriteLine("Common wear ");
+
+        }
+    }
+
+    public class ClimateControl : Clothing
+    {
+        public override void Sales()
+        {
+            Console.WriteLine("Sales by region for ClimateControl clothing:");
+        }
+
+        public override void Materials()
+        {
+            Console.WriteLine("Materials");
+
+        }
+
+        public override void CommonWear()
+        {
+            Console.WriteLine("Common Wear");
+
+        }
+    }
+
+    public class Sports : Clothing
+    {
+        public override void Sales()
+        {
+            Console.WriteLine("Sales");
+
+        }
+
+        public override void Materials()
+        {
+            Console.WriteLine("Materials");
+
+        }
+
+        public override void CommonWear()
+        {
+            Console.WriteLine("Common wear");
+
+        }
+    }
+    public static void Main(string[] args)
+    {
+        //..........Assignment 3
+        TrendSetting trendSetting = new TrendSetting();
+        trendSetting.Sales();
+        trendSetting.Materials();
+        trendSetting.CommonWear();
+
+        ClimateControl climateControl = new ClimateControl();
+        climateControl.Sales();
+        climateControl.Materials();
+        climateControl.CommonWear();
+
+        Sports sports = new Sports();
+        sports.Sales();
+        sports.Materials();
+        sports.CommonWear();
+    }
+
+}
